09d1dcbe290b94671ffa61a81a0104945eee7983
ilyasAbdi01 <ilyasyr@gmail.com>
2023-02-13 17:21:52 +0300

    another change

--- 01_basics/01_hello.py
+++ 01_basics/01_hello.py
@@ -1,1 +1,17 @@
 print("HELLO , WORLD")
+print("HELLO , WORLD")
+
+# getting user input
+user_name = input ("Enter your name:  ")
+print(user_name)
+print(f"Your name is {user_name}")
+print("Your name is "+user_name)
+print("Your name is %s"%user_name)
+
+# getting integer input
+phone_number = int(input("Enter your number: "))
+print(phone_number)
+print("The number is {phone_number}")
+
+fraction = float(input("Enter a float:  "))
+print(f"The number is {fraction}")
