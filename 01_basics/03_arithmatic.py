1f136dddfd2a56872b62496682c81db73ab20ded
ilyasAbdi01 <ilyasyr@gmail.com>
2023-02-28 14:36:37 +0300

    arithmatic

---
+++ 01_basics/03_arithmatic.py
@@ -0,0 +1,13 @@
+def arithmatic():
+    num1 = int(input('Enter the first number:  '))
+    num2 = int (input('Enter the second number:  '))
+
+    add = num1 + num2
+    sub = num1 - num2
+    multi = num2 * num1
+    div = num1 / num2
+    mod = num1 % num2
+
+    print(f'Result {add} \n {sub} \n {multi} \n {div} \n {mod} \n')
+
+arithmatic()    
